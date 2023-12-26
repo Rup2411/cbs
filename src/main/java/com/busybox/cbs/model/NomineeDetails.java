df2cc4da2b30e217fc37b044efd88f0ed7617211
BusyboxDevelopers <Developers@busybox.in>
2023-12-26 14:25:25 +0530

    nominee

--- src/main/java/com/busybox/cbs/model/NomineeDetails.java
+++ src/main/java/com/busybox/cbs/model/NomineeDetails.java
@@ -1,11 +1,54 @@
 package com.busybox.cbs.model;
 
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.busybox.cbs.model.enums.FamilyRelation;
+import com.busybox.cbs.model.enums.NomineeKYCType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 @Entity
 @Data
 public class NomineeDetails {
 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String Name;
+	private FamilyRelation nomineeRelation;
+	private String Address;
+	private String kycNumber;
+	private String MobileNumber;
+	private String Age;
+	private NomineeKYCType kycType;
+	
+	
+	
+	// Date time created on/ updated on
+		@JsonIgnore
+	    @CreationTimestamp
+	    @Temporal(TemporalType.TIMESTAMP)
+	    private LocalDateTime CreatedOn; 
+	    @JsonIgnore
+	    private String CreatedBy;
+	    
+	    @JsonIgnore
+	    @UpdateTimestamp
+	    @Temporal(TemporalType.TIMESTAMP)
+	    private LocalDateTime UpdatedOn;
+	    
+	    @JsonIgnore
+	    private String UpdatedBy;
 }
---
+++ src/main/java/com/busybox/cbs/model/enums/NomineeKYCType.java
@@ -0,0 +1,9 @@
+package com.busybox.cbs.model.enums;
+
+public enum NomineeKYCType {
+
+	Aadhar,
+	Voter,
+	Ration_Card,
+	Driving_License;
+}

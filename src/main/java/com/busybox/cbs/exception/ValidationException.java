d20e610c036c7fb0815a5e7c6e4e763534eff1a9
BusyboxDevelopers <Developers@busybox.in>
2023-12-26 19:02:50 +0530

    push

--- src/main/java/com/busybox/cbs/exception/ValidationException.java
+++ src/main/java/com/busybox/cbs/exception/ValidationException.java
@@ -1,9 +1,6 @@
 package com.busybox.cbs.exception;
 
 public class ValidationException extends RuntimeException {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public ValidationException(String message) {
